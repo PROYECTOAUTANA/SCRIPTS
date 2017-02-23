706ac72991831be459a431ee2b3c592ab5e132ba
yordy <yordyalejandro_14@hotmail.com>
2017-02-23 11:32:55 -0400

    Scripts Tablas

--- fases.sql
+++ fases.sql
@@ -1,22 +1,19 @@
--- Table: public.docente
+-- Table: public.fases
 
--- DROP TABLE public.docente;
+-- DROP TABLE public.fases;
 
-CREATE TABLE public.docente
+CREATE TABLE public.fases
 (
-    pk_id_docente bigint NOT NULL DEFAULT nextval('docente_pk_id_docente_seq'::regclass),
-    nombre character(30) COLLATE pg_catalog."default",
-    apellido character(30) COLLATE pg_catalog."default",
-    cedula character(30) COLLATE pg_catalog."default",
-    telefono character(30) COLLATE pg_catalog."default",
-    correo character(1) COLLATE pg_catalog."default",
-    fk_categorias bigint,
-    CONSTRAINT docente_pkey PRIMARY KEY (pk_id_docente)
+    pk_id_fases bigint NOT NULL DEFAULT nextval('fases_pk_id_fases_seq'::regclass),
+    f_inicio character(30) COLLATE pg_catalog."default",
+    f_finalizacion character(30) COLLATE pg_catalog."default",
+    fk_id_trabajo bigint,
+    CONSTRAINT fases_pkey PRIMARY KEY (pk_id_fases)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.docente
+ALTER TABLE public.fases
     OWNER to postgres;
